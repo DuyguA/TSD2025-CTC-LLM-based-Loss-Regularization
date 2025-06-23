43d2f5e8a42e077f3258fed62543d0f5dc6ccf35
DuyguA <duygu.altinok12@gmail.com>
2025-06-23 18:56:21 +0200

    added model code

--- experiment-makers/models.py
+++ experiment-makers/models.py
@@ -3,14 +3,14 @@ import torch.nn as nn
 from transformers import Wav2Vec2Processor, Wav2Vec2ConformerCTC
 
 class IntermediateWav2Vec2CTC(nn.Module):
-    def __init__(self, wav2vec_model, llm_dim):
+    def __init__(self, wav2vec_model, num_heads, llm_dim):
         super(IntermediateWav2VecCTC, self).__init__()
         
+        self.num_heads = num_heads
         self.model = wav2vec_model
         self.model.config.output_hidden_states = True
         encoder_dim = self.model.hidden_size
-        self.projection_layers = nn.ModuleList([nn.Linear(encoder_dim, llm_dim)
-                                           for _ in range(4)]) # 4 intermediate heads
+        self.projection_layers = nn.ModuleList([nn.Linear(encoder_dim, llm_dim) for _ in range(self.num_heads)]) # 4 intermediate heads
 
 
     def forward(self, input_ids, attention_mask):
@@ -19,7 +19,7 @@ class IntermediateWav2Vec2CTC(nn.Module):
         hidden_states = outputs.hidden_states
 
         inters = [6, 12, 18, 24]
-        intermediate_outputs  = [self.projection_layers[i](hidden_states[inters[i]]) for i in range(4)]
+        intermediate_outputs  = [self.projection_layers[i](hidden_states[inters[i]]) for i in range(self.num_heads)]
 
         return {
             "logits": outputs.logits, # Final CTC logits
